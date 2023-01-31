981545424eee891331c9b35fdfb77dd33b658aa4
IvanchikAleksandr <Ivanchikaleksandr11@gmail.com>
2023-01-31 16:30:35 +0300

    homeworktest

---
+++ src/test/java/HomeWorkTest.java
@@ -0,0 +1,31 @@
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class HomeWorkTest {
+
+    public double getTax(double income) {
+        if (income <10000 ){
+            return income * 0.13;} else if (income >= 10000 && income < 50000 ) {
+            return income * 0.2;
+        } else { return income * 0.3;
+            
+        }
+
+    }
+
+     @Test(dataProvider = "incomeprovider")
+     public void checkTexesCalculation(int income, double expTax){
+         Assert.assertEquals(getTax(income), expTax * income,String.format("Texes for %s should be %s", income, expTax));
+     }
+     @DataProvider
+     public Object[][] incomeprovider(){return new Object[][]{
+             {0,0},
+             {9999,0.13},
+             {10000,0.2},
+             {49999,0.2},
+             {50000,0.3},
+     };
+
+    }
+}
